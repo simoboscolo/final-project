83155666af7be1a32c5108f415d35a87f23643ca
Simone Boscolo <simoboscolo@icloud.com>
2016-03-27 10:18:28 +0200

    layout ready, all content missing

---
+++ styles/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+	$("#dayOnePop").hide(0);
+
+	$("#dayOne").click(function(){
+		$("#dayOnePop").fadeIn(500);
+	});
+
+	$("#dayOneHide").click(function(){
+		$("#dayOnePop").fadeOut(500);
+	})
+
+});
