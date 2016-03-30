bf9fb82e73a3a158b955a35092e1f7dd815679f4
Simone Boscolo <simoboscolo@icloud.com>
2016-03-31 00:06:32 +0200

    scroll in responsive for text and jQuery ok

--- styles/script.js
+++ styles/script.js
@@ -1,13 +1,64 @@
 $(document).ready(function(){
 
 	$("#dayOnePop").hide(0);
+	$("#dayTwoPop").hide(0);
+
+// DAY ONE
 
 	$("#dayOne").click(function(){
-		$("#dayOnePop").fadeIn(500);
+
+		$("#dayOnePop").fadeIn(800);
+
+		function changeImg(selector, image) {
+			selector.attr("src", "imgs/day1/" + image + ".jpg");
+		}
+
+		var sel = $("#dayOneImg");
+		var images = ["1", "2", "3", "4", "5"];
+		var i = 0;
+
+		setInterval(function(){
+			changeImg(sel, images[i]);
+			if (i == 4) {
+				i = 0;
+			} else {
+				i++;
+			}
+		}, 5000);
+	
 	});
 
 	$("#dayOneHide").click(function(){
-		$("#dayOnePop").fadeOut(500);
-	})
+		$("#dayOnePop").fadeOut(800);
+	});
+
+// DAY TWO
+
+	$("#dayTwo").click(function(){
+
+		$("#dayTwoPop").fadeIn(800);
+
+		function changeImg2(selector, image) {
+			selector.attr("src", "imgs/day2/" + image + ".jpg");
+		}
+
+		var sel = $("#dayTwoImg");
+		var images = ["1", "2", "3", "4", "5"];
+		var i = 0;
+
+		setInterval(function(){
+			changeImg2(sel, images[i]);
+			if (i == 4) {
+				i = 0;
+			} else {
+				i++;
+			}
+		}, 5000);
+	
+	});
+
+	$("#dayTwoHide").click(function(){
+		$("#dayTwoPop").fadeOut(800);
+	});
 
 });
