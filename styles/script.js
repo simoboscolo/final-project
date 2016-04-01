7cd82f58b8db7fa30bc23caa1165aadceab4ff74
Simone Boscolo <simoboscolo@icloud.com>
2016-04-01 11:37:19 +0200

    addes days 3 and 4 + fixed issue with picture loop that started again on click

--- styles/script.js
+++ styles/script.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
 
 	$("#dayOnePop").hide(0);
 	$("#dayTwoPop").hide(0);
+	$("#dayThreePop").hide(0);
+	$("#dayFourPop").hide(0);
 
 // DAY ONE
 
@@ -17,7 +19,7 @@ $(document).ready(function(){
 		var images = ["1", "2", "3", "4", "5"];
 		var i = 0;
 
-		setInterval(function(){
+		var intervalId = setInterval(function(){
 			changeImg(sel, images[i]);
 			if (i == 4) {
 				i = 0;
@@ -25,11 +27,12 @@ $(document).ready(function(){
 				i++;
 			}
 		}, 5000);
-	
-	});
 
-	$("#dayOneHide").click(function(){
-		$("#dayOnePop").fadeOut(800);
+		$("#dayOneHide").click(function(){
+			clearInterval(intervalId);
+			$("#dayOnePop").fadeOut(800);
+		});
+	
 	});
 
 // DAY TWO
@@ -46,7 +49,7 @@ $(document).ready(function(){
 		var images = ["1", "2", "3", "4", "5"];
 		var i = 0;
 
-		setInterval(function(){
+		var intervalId = setInterval(function(){
 			changeImg2(sel, images[i]);
 			if (i == 4) {
 				i = 0;
@@ -54,11 +57,73 @@ $(document).ready(function(){
 				i++;
 			}
 		}, 5000);
+
+		$("#dayTwoHide").click(function(){
+			clearInterval(intervalId);
+			$("#dayTwoPop").fadeOut(800);
+		});
 	
 	});
 
-	$("#dayTwoHide").click(function(){
-		$("#dayTwoPop").fadeOut(800);
+
+// DAY THREE
+
+	$("#dayThree").click(function(){
+
+		$("#dayThreePop").fadeIn(800);
+
+		function changeImg3(selector, image) {
+			selector.attr("src", "imgs/day3/" + image + ".jpg");
+		}
+
+		var sel = $("#dayThreeImg");
+		var images = ["1", "2", "3", "4", "5"];
+		var i = 0;
+
+		var intervalId = setInterval(function(){
+			changeImg3(sel, images[i]);
+			if (i == 4) {
+				i = 0;
+			} else {
+				i++;
+			}
+		}, 5000);
+
+		$("#dayThreeHide").click(function(){
+			clearInterval(intervalId);
+			$("#dayThreePop").fadeOut(800);
+		});
+	
+	});
+
+// DAY Four
+
+	$("#dayFour").click(function(){
+
+		$("#dayFourPop").fadeIn(800);
+
+		function changeImg4(selector, image) {
+			selector.attr("src", "imgs/day4/" + image + ".jpg");
+		}
+
+		var sel = $("#dayFourImg");
+		var images = ["1", "2", "3", "4", "5"];
+		var i = 0;
+
+		var intervalId = setInterval(function(){
+			changeImg4(sel, images[i]);
+			if (i == 4) {
+				i = 0;
+			} else {
+				i++;
+			}
+		}, 5000);
+
+		$("#dayFourHide").click(function(){
+			clearInterval(intervalId);
+			$("#dayFourPop").fadeOut(800);
+		});
+	
 	});
 
 });
